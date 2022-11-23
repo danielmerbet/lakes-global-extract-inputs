88b7bbd344f8dbb7705c4552b56fd513cea8de84
Matthias Buechner <m.buechner@gmail.com>
2022-11-23 10:56:47 +0100

    process all variables

--- extract_lakes.py
+++ extract_lakes.py
@@ -54,20 +54,20 @@ for period in get_periods(path):
     # get file names for current period
     tas_file = [file for file in fnmatch.filter(os.listdir(path), '*_tas_*') if period in file][0]
     hurs_file = [file for file in fnmatch.filter(os.listdir(path), '*_hurs_*') if period in file][0]
-    # pr_file = [file for file in fnmatch.filter(os.listdir(path), '*_pr_*') if period in file][0]
-    # rsds_file = [file for file in fnmatch.filter(os.listdir(path), '*_rsds_*') if period in file][0]
-    # rlds_file = [file for file in fnmatch.filter(os.listdir(path), '*_rlds_*') if period in file][0]
-    # ps_file = [file for file in fnmatch.filter(os.listdir(path), '*_ps_*') if period in file][0]
-    # sfcwind_file = [file for file in fnmatch.filter(os.listdir(path), '*_sfcwind_*') if period in file][0]
+    pr_file = [file for file in fnmatch.filter(os.listdir(path), '*_pr_*') if period in file][0]
+    rsds_file = [file for file in fnmatch.filter(os.listdir(path), '*_rsds_*') if period in file][0]
+    rlds_file = [file for file in fnmatch.filter(os.listdir(path), '*_rlds_*') if period in file][0]
+    ps_file = [file for file in fnmatch.filter(os.listdir(path), '*_ps_*') if period in file][0]
+    sfcwind_file = [file for file in fnmatch.filter(os.listdir(path), '*_sfcwind_*') if period in file][0]
 
     # open datasets
     tas_ds = Dataset(path + '/' + tas_file)
     hurs_ds = Dataset(path + '/' + hurs_file)
-    # pr_ds = Dataset(path + '/' + pr_file)
-    # rsds_ds = Dataset(path + '/' + rsds_file)
-    # rlds_ds = Dataset(path + '/' + rlds_file)
-    # ps_ds = Dataset(path + '/' + ps_file)
-    # sfcwind_ds = Dataset(path + '/' + sfcwind_file)
+    pr_ds = Dataset(path + '/' + pr_file)
+    rsds_ds = Dataset(path + '/' + rsds_file)
+    rlds_ds = Dataset(path + '/' + rlds_file)
+    ps_ds = Dataset(path + '/' + ps_file)
+    sfcwind_ds = Dataset(path + '/' + sfcwind_file)
 
     # get dates
     times = tas_ds.variables['time']
@@ -90,26 +90,26 @@ for period in get_periods(path):
         tas_lake = tas_ds['tas'][:, idx_lat, idx_lon]
         print('   read hurs ...')
         hurs_lake = hurs_ds['hurs'][:, idx_lat, idx_lon]
-        # print('   read pr ...')
-        # pr_lake = pr_ds['pr'][:, idx_lat, idx_lon]
-        # print('   read rsds ...')
-        # rsds_lake = rsds_ds['rsds'][:, idx_lat, idx_lon]
-        # print('   read rlds ...')
-        # rlds_lake = rlds_ds['rlds'][:, idx_lat, idx_lon]
-        # print('   read ps ...')
-        # ps_lake = ps_ds['ps'][:, idx_lat, idx_lon]
-        # print('   read sfcwind ...')
-        # sfcwind_lake = sfcwind_ds['sfcwind'][:, idx_lat, idx_lon]
+        print('   read pr ...')
+        pr_lake = pr_ds['pr'][:, idx_lat, idx_lon]
+        print('   read rsds ...')
+        rsds_lake = rsds_ds['rsds'][:, idx_lat, idx_lon]
+        print('   read rlds ...')
+        rlds_lake = rlds_ds['rlds'][:, idx_lat, idx_lon]
+        print('   read ps ...')
+        ps_lake = ps_ds['ps'][:, idx_lat, idx_lon]
+        print('   read sfcwind ...')
+        sfcwind_lake = sfcwind_ds['sfcwind'][:, idx_lat, idx_lon]
 
         lake_data = pd.DataFrame({
             'date': dates[:],
             'tas': tas_lake[:],
             'hurs': hurs_lake[:],
-            # 'pr': pr_lake[:],
-            # 'rsds': rsds_lake[:],
-            # 'rlds': rlds_lake[:],
-            # 'ps': ps_lake[:],
-            # 'sfcwind': sfcwind_lake[:]
+            'pr': pr_lake[:],
+            'rsds': rsds_lake[:],
+            'rlds': rlds_lake[:],
+            'ps': ps_lake[:],
+            'sfcwind': sfcwind_lake[:]
         })
 
         # write csv files per lake
@@ -119,14 +119,10 @@ for period in get_periods(path):
         else:
             lake_data.to_csv(outpath / outfile, encoding='utf-8', float_format='%.1f', index=False, mode='a', header=False)
 
-        # breal after first lake for faster debugging
-        # break
-    break
-
     tas_ds.close()
     hurs_ds.close()
-    # pr_ds.close()
-    # rsds_ds.close()
-    # rlds_ds.close()
-    # ps_ds.close()
-    # sfcwind_ds.close()
+    pr_ds.close()
+    rsds_ds.close()
+    rlds_ds.close()
+    ps_ds.close()
+    sfcwind_ds.close()
--- extract_lakes_xarray.py
+++ extract_lakes_xarray.py
@@ -54,20 +54,20 @@ for period in get_periods(path):
     # get file names for current period
     tas_file = [file for file in fnmatch.filter(os.listdir(path), '*_tas_*') if period in file][0]
     hurs_file = [file for file in fnmatch.filter(os.listdir(path), '*_hurs_*') if period in file][0]
-    # pr_file = [file for file in fnmatch.filter(os.listdir(path), '*_pr_*') if period in file][0]
-    # rsds_file = [file for file in fnmatch.filter(os.listdir(path), '*_rsds_*') if period in file][0]
-    # rlds_file = [file for file in fnmatch.filter(os.listdir(path), '*_rlds_*') if period in file][0]
-    # ps_file = [file for file in fnmatch.filter(os.listdir(path), '*_ps_*') if period in file][0]
-    # sfcwind_file = [file for file in fnmatch.filter(os.listdir(path), '*_sfcwind_*') if period in file][0]
+    pr_file = [file for file in fnmatch.filter(os.listdir(path), '*_pr_*') if period in file][0]
+    rsds_file = [file for file in fnmatch.filter(os.listdir(path), '*_rsds_*') if period in file][0]
+    rlds_file = [file for file in fnmatch.filter(os.listdir(path), '*_rlds_*') if period in file][0]
+    ps_file = [file for file in fnmatch.filter(os.listdir(path), '*_ps_*') if period in file][0]
+    sfcwind_file = [file for file in fnmatch.filter(os.listdir(path), '*_sfcwind_*') if period in file][0]
 
     # open datasets
     tas_ds = xr.load_dataset(path + '/' + tas_file)
     hurs_ds = xr.load_dataset(path + '/' + hurs_file)
-    # pr_ds = xr.load_dataset(path + '/' + pr_file)
-    # rsds_ds = xr.load_dataset(path + '/' + rsds_file)
-    # rlds_ds = xr.load_dataset(path + '/' + rlds_file)
-    # ps_ds = xr.load_dataset(path + '/' + ps_file)
-    # sfcwind_ds = xr.load_dataset(path + '/' + sfcwind_file)
+    pr_ds = xr.load_dataset(path + '/' + pr_file)
+    rsds_ds = xr.load_dataset(path + '/' + rsds_file)
+    rlds_ds = xr.load_dataset(path + '/' + rlds_file)
+    ps_ds = xr.load_dataset(path + '/' + ps_file)
+    sfcwind_ds = xr.load_dataset(path + '/' + sfcwind_file)
 
     # iterate over lakes
     for index, lake in lakes_df.iterrows():
@@ -82,30 +82,28 @@ for period in get_periods(path):
         print('   read hurs ...')
         hurs_lake = hurs_ds.sel(lat=lake[1], lon=lake[2], method='nearest')
         print('   read pr ...')
-        # pr_lake = pr_ds.sel(lat=lake[1], lon=lake[2], method='nearest')
-        # print('   read rsds ...')
-        # rsds_lake = rsds_ds.sel(lat=lake[1], lon=lake[2], method='nearest')
-        # print('   read rlds ...')
-        # rlds_lake = rlds_ds.sel(lat=lake[1], lon=lake[2], method='nearest')
-        # print('   read ps ...')
-        # ps_lake = ps_ds.sel(lat=lake[1], lon=lake[2], method='nearest')
-        # print('   read sfcwind ...')
-        # sfcwind_lake = sfcwind_ds.sel(lat=lake[1], lon=lake[2], method='nearest')
+        pr_lake = pr_ds.sel(lat=lake[1], lon=lake[2], method='nearest')
+        print('   read rsds ...')
+        rsds_lake = rsds_ds.sel(lat=lake[1], lon=lake[2], method='nearest')
+        print('   read rlds ...')
+        rlds_lake = rlds_ds.sel(lat=lake[1], lon=lake[2], method='nearest')
+        print('   read ps ...')
+        ps_lake = ps_ds.sel(lat=lake[1], lon=lake[2], method='nearest')
+        print('   read sfcwind ...')
+        sfcwind_lake = sfcwind_ds.sel(lat=lake[1], lon=lake[2], method='nearest')
 
         # write csv files per lake
         print('   write data ...')
+
         lake_data = tas_lake.to_dataframe()
         lake_data['hurs'] = hurs_lake['hurs']
-        # lake_data['pr'] = hurs_lake['pr']
-        # lake_data['rsds'] = hurs_lake['rsds']
-        # lake_data['rlds'] = hurs_lake['rlds']
-        # lake_data['ps'] = hurs_lake['ps']
-        # lake_data['sfcwind'] = hurs_lake['sfcwind']
+        lake_data['pr'] = hurs_lake['pr']
+        lake_data['rsds'] = hurs_lake['rsds']
+        lake_data['rlds'] = hurs_lake['rlds']
+        lake_data['ps'] = hurs_lake['ps']
+        lake_data['sfcwind'] = hurs_lake['sfcwind']
+
         if period.split('_')[0] == first_year:
             lake_data.to_csv(outpath / outfile)
         else:
             lake_data.to_csv(outpath / outfile, mode='a', header=False)
-
-        # break after first lake for faster debugging
-        # break
-    break
